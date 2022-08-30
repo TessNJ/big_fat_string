f97b0f0e9c9c211d6045aae41362ab0c67c581f8
Tess <tere0506@stud.kea.dk>
2022-08-30 09:27:56 +0200

    done_for_now

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ let dropId;
 let dropResult;
 let outputId;
 let outputNew;
+let dropChange;
+let textArray;
+let outputText;
+let startIndex;
+let endIndex;
 
 function readInput() {
   inputText = document.getElementById("newInput").value;
@@ -19,14 +24,102 @@ function dropDownF() {
 
 function dropHandle() {
   console.log("here: " + inputText + " and " + dropResult);
-  if (dropResult == 1) {
-    console.log("nice");
+  if (dropResult == 1 && inputText.split(" ").length == 1) {
+    drop1();
+  } else if (dropResult == 2 && inputText.split(" ").length >= 3) {
+    drop2();
+  } else if (dropResult == 3 && inputText.split(" ").length >= 3) {
+    drop3();
+  } else if (dropResult == 4 && inputText.split(" ").length >= 3) {
+    drop4();
+  } else if (
+    dropResult == 5 &&
+    inputText
+      .split(/(\\|\/)/g)
+      .pop()
+      .includes(".")
+  ) {
+    drop5();
+  } else if (dropResult == 6 && !inputText.includes(" ")) {
+    drop6();
+  } else if (dropResult == 7) {
+    console.log("hi");
+    drop7();
+  } else if (dropResult == 8) {
+    drop8();
   } else {
     console.log("Error: Not Coded yet");
   }
+  //   appendOut();
+}
+//Append
+function appendOut() {
+  document.getElementById("newOutput").value = outputText;
+}
+
+//Drop 1 section
+
+function drop1() {
+  outputText = inputText.charAt(0).toUpperCase() + inputText.slice(1);
+  console.log(outputText);
   appendOut();
 }
 
-function appendOut() {
-  document.getElementById("newOutput").value = inputText;
+//Drop 2
+function drop2() {
+  textArray = inputText.split(" ");
+  outputText = textArray[0];
+  console.log(outputText);
+  appendOut();
 }
+
+//Drop 3
+function drop3() {
+  textArray = inputText.split(" ");
+  outputText = textArray[0].length;
+  appendOut();
+}
+
+//Drop 4 ///NEED HELP///
+function drop4() {
+  textArray = inputText.split(" ");
+  outputText = textArray[1];
+  startIndex = inputText.indexOf(textArray[1]);
+  endIndex = startIndex + outputText.length;
+  outputText = `"${outputText}" starts at ${startIndex} and ends at ${endIndex}`;
+  appendOut();
+}
+
+//Drop 5
+function drop5() {
+  outputText = inputText.split(/(\\|\/)/g).pop();
+  if (outputText.includes(".png")) {
+    outputText = `"${outputText}" includes .png`;
+  } else if (outputText.includes(".jpg")) {
+    outputText = `"${outputText}" includes .jpg`;
+  } else {
+    outputText = `"${outputText}" includes neither .png or .jpg`;
+  }
+  appendOut();
+}
+//Drop 6
+
+function drop6() {
+  // outputText = inputText.padEnd(inputText.length, "*");
+  outputText = inputText.replace(/./g, "*");
+  console.log(outputText);
+  appendOut();
+}
+//Drop 7
+function drop7() {
+  textArray = inputText.split(" ");
+
+  // for (let outputText of textArray) {
+  //   outputText = outputText.charAt(2).replace(toUpperCase);
+  //   console.log(outputText);
+  // }
+  // appendOut();
+}
+
+//Drop 8
+function drop8() {}
